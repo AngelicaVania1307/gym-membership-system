dac7d184e598e39d7b69fa9bd0389ec64adb865b
drenaton <vanesusilo@gmail.com>
2026-05-23 15:43:27 +0700

    feat: implement premium membership

--- src/membership.py
+++ src/membership.py
@@ -1,2 +1,11 @@
-def calculate_membership_fee():
-    pass
+def calculate_membership_fee(
+    age,
+    membership_type,
+    access_day,
+    membership_duration
+):
+
+    if membership_type == "premium":
+        return "Unlimited Access"
+
+    return 50
